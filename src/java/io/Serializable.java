8e3aa4166329cc7190bf80c6e3da211e852df0d0
Charles Bentley <cbentley@mail.ru>
2024-08-14 08:51:40 +0200

    refactor package name 08/2024

--- src/java/io/Serializable.java
+++ src/java/io/Serializable.java
@@ -1,5 +1,15 @@
 package java.io;
 
+/**
+ * Why is this needed ?
+ * 
+ * This is done to allow class in utils to implements Serializable and stay compatible with J2ME class that do not.
+ * 
+ * One such class. Investigate. I forgot.
+ * 
+ * @author Charles Bentley
+ *
+ */
 public interface Serializable {
 
 }
